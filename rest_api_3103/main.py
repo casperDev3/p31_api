dac8075eea27a1c51a4f2c681aab929d16ecab7d
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-03-31 19:39:13 +0300

    [auto_save] Push completed code

--- rest_api_3103/main.py
+++ rest_api_3103/main.py
@@ -6,6 +6,51 @@ import uvicorn
 
 app = FastAPI(title="REST FastAPI", version="0.0.1")
 
+def log_execution_time(endpoint_name: str):
+    def decorator(func):
+        # TODO: next lesson
+        async def wrapper(*args, **kwargs):
+            start_time = datetime.now()
+            result = await func(*args, **kwargs)
+            end_time = datetime.now()
+            execution_time = (end_time - start_time).total_seconds()
+            print(f"Endpoint '{endpoint_name}' executed in {execution_time:.4f} seconds")
+            return result
+        return wrapper
+    return decorator
+
+# models #TODO: next lesson
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    age: int
+
+class User(UserCreate):
+    id: int
+
+
+# local db
+users_db: List[User] = []
+user_id_counter = 1
+
+#TODO: next lesson
+def user_id_generator() -> Generator[int, None, None]:
+    global user_id_counter
+    while True:
+        yield user_id_counter
+        user_id_counter += 1
+
+# lambda filters #TODO: next lesson
+filter_adults = lambda users: [user for user in users if user.age >= 18]
+filter_by_name = lambda name: lambda users: [user for user in users if user.name.lower() == name.lower()]
+
+# 404
+def get_user_or_404(user_id: int) -> User:
+    user = next((user for user in users_db if user.id == user_id), None)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
