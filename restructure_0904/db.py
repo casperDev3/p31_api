16f6fd467f1a4cd10f46f7a544f8d26ffb29f9bd
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-04-09 20:54:37 +0300

    [auto_save] Push completed code

--- restructure_0904/db.py
+++ restructure_0904/db.py
@@ -1,11 +1,15 @@
 from datetime import datetime
+import security
+from passlib.context import CryptContext
+
+hash_psw_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 fake_users_db = {
     "admin": {
         "email": "admin@example.com",
         "username": "admin",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW", # admin123
-        "role": "admin",
+        "hashed_password": hash_psw_context.hash("admin123"), # admin123
+        "is_admin": True,
         "created_at": datetime.utcnow()
     }
 }
--- restructure_0904/schemas/__init__.py
+++ restructure_0904/schemas/__init__.py
@@ -1,2 +1,12 @@
 from .auth import UserRegister, UserLogin, UserResponse, ChangePassword, ForgotPassword, ResetPassword
 from .users import UpdateProfile
+
+__all__ = [
+    "UserRegister",
+    "UserLogin",
+    "UserResponse",
+    "ChangePassword",
+    "ForgotPassword",
+    "ResetPassword",
+    "UpdateProfile"
+]
