e088fb9a485b4d87839885ee38fecc8233f6d6d9
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-04-21 20:23:54 +0300

    [auto_save] Push completed code

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Article
 
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_date')
@@ -9,4 +10,13 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('created_date',)
 
     readonly_fields = ('created_date', 'id')
-    fields = ('id', 'title', 'content', 'created_date')
+    fields = (
+        'id',
+        'title',
+        'content',
+        'created_date',
+        'author',
+        'image',
+        'tags',
+        'category'
+    )
--- news/models.py
+++ news/models.py
@@ -5,5 +5,11 @@ class Article(models.Model):
     content = models.TextField(verbose_name="Текст новини")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата створення")
 
+    # Optional fields for future use
+    author = models.CharField(max_length=100, verbose_name="Автор", null=True)
+    image = models.ImageField(upload_to='articles/', null=True, blank=True, verbose_name="Зображення")
+    tags = models.CharField(max_length=200, null=True, blank=True, verbose_name="Теги", help_text="Введіть теги через кому")
+    category = models.CharField(max_length=100, null=True, blank=True, verbose_name="Категорія")
+
     def __str__(self):
         return self.title
