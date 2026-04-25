715b38adeeaa92e73c17e62ba4d68fb06d305671
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-04-25 11:30:50 +0300

    [auto_save] Push completed code

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Article, Category, Tag
 from .serializers import ArticleSerializer, CategorySerializer, TagSerializer
 
@@ -6,6 +7,14 @@ from .serializers import ArticleSerializer, CategorySerializer, TagSerializer
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all().order_by('-created_date')
     serializer_class = ArticleSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['category', 'author'] # Точний фільтр за категорією та автором
+    search_fields = ['title', 'content'] # Пошук за заголовком та вмістом
+    ordering_fields = ['created_date', 'title'] # Сортування за датою створення та заголовком
+    ordering = ['-created_date'] # За замовчуванням сортувати за датою створення у спадному порядку
+
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
