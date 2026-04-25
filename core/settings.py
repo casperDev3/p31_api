08f336b1f207e2c2d370034e63c5e74593f673b8
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-04-25 12:11:13 +0300

    [auto_save] Push completed code

--- core/settings.py
+++ core/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     # installed apps
     'rest_framework',
     'django_filters',
+    'rest_framework_simplejwt',
     # custom apps
-    'news'
+    'news',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ PREFIX = 'api/v1'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(f'{PREFIX}/news/', include('news.urls')),
+    path(f'{PREFIX}/auth/', include('authentication.urls')),
 ]
