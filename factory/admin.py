7cdd5269ea116c912833470a88be9ff3bdf546ec
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-05-05 20:26:41 +0300

    [auto_save] Push completed code

--- factory/admin.py
+++ factory/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import WorkShop, Worker, Attendance, Shipment
 
-# Register your models here.
+@admin.register(WorkShop)
+class WorkShopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'position', 'workshop')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'position')
+    list_filter = ('workshop',)
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'worker', 'date', 'clock_in')
+    list_display_links = ('id',)
+    search_fields = ('worker__name',)
+    list_filter = ('date',)
+
+@admin.register(Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'status')
+    list_display_links = ('id', 'description')
+    search_fields = ('description',)
+    list_filter = ('status',)
--- factory/schema.py
+++ factory/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from django.contrib.messages import success
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from .models import WorkShop, Worker, Attendance, Shipment
@@ -27,6 +28,7 @@ class Query(graphene.ObjectType):
     all_workers = graphene.List(WorkerType)
     workshop_by_name = graphene.Field(WorkShopType, name=graphene.String(required=True))
     worker_filter = DjangoFilterConnectionField(WorkerNode)
+    one_worker = graphene.Field(WorkerType, id=graphene.ID(required=True))
 
     def resolve_all_workers(root, info):
         return Worker.objects.select_related('workshop').all()
@@ -35,6 +37,12 @@ class Query(graphene.ObjectType):
         try:
             return WorkShop.objects.get(name=name)
         except WorkShop.DoesNotExist:
+            return None
+
+    def resolve_one_worker(root,  info, id):
+        try:
+            return Worker.objects.get(pk=id)
+        except Worker.DoesNotExist:
             return None
 
 # --     GraphQL Mutations
@@ -47,8 +55,76 @@ class CreateAttendanceMutation(graphene.Mutation):
     @classmethod
     def mutate(cls, root, info, worker_id):
         worker = Worker.objects.get(pk=worker_id)
-        Attendance.objects.create(worker=worker)
-        return CreateAttendanceMutation().success
+        Attendance.objects.create(worker=worker, date=graphene.Date(), clock_in=graphene.Time())
+        return CreateAttendanceMutation(success=True)
+
+class CreateWorkerMutation(graphene.Mutation):
+    id = graphene.ID(required=True)
+    name = graphene.String()
+    position = graphene.String()
+    workshop = graphene.Field(WorkShopType)
+
+    class Arguments:
+        name = graphene.String(required=True)
+        position = graphene.String(required=True)
+        workshop_id = graphene.ID(required=True)
+
+    @classmethod
+    def mutate(cls, root, info, name, position, workshop_id):
+        workshop = WorkShop.objects.get(pk=workshop_id)
+        worker = Worker.objects.create(name=name, position=position, workshop=workshop)
+        return CreateWorkerMutation(
+            id=worker.id,
+            name=worker.name,
+            position=worker.position,
+            workshop=workshop
+        )
+
+class UpdateWorkerMutation(graphene.Mutation):
+    id = graphene.ID(required=True)
+    name = graphene.String()
+    position = graphene.String()
+    workshop = graphene.Field(WorkShopType)
+
+    class Arguments:
+        id = graphene.ID(required=True)
+        name = graphene.String()
+        position = graphene.String()
+        workshop_id = graphene.ID()
+
+    @classmethod
+    def mutate(cls, root, info, id, name=None, position=None, workshop_id=None):
+        worker = Worker.objects.get(pk=id)
+        if name:
+            worker.name = name
+        if position:
+            worker.position = position
+        if workshop_id:
+            workshop = WorkShop.objects.get(pk=workshop_id)
+            worker.workshop = workshop
+        worker.save()
+        return UpdateWorkerMutation(
+            id=worker.id,
+            name=worker.name,
+            position=worker.position,
+            workshop=worker.workshop
+        )
+
+class DeleteWorkerMutation(graphene.Mutation):
+    id = graphene.ID(required=True)
+    is_deleted = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        worker = Worker.objects.get(pk=id)
+        worker.delete()
+        return DeleteWorkerMutation(id=id, is_deleted=True)
 
 class Mutation(graphene.ObjectType):
     make_attendance = CreateAttendanceMutation.Field()
+    create_worker = CreateWorkerMutation.Field()
+    update_worker = UpdateWorkerMutation.Field()
+    delete_worker = DeleteWorkerMutation.Field()
