0b0cdfca2e236f95a9349a36f838b74627157701
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-05-12 20:31:10 +0300

    [auto_save] Push completed code

--- news/client_urls.py
+++ news/client_urls.py
@@ -3,6 +3,6 @@ from .views import LandingView, ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='news'),
-    path('/<int:pk>/', ArticleDetailView.as_view(), name='news-detail'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='news-detail'),
     path('landing/', LandingView.as_view(), name='landing'),
 ]
